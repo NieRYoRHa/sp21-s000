4e60f803c0a1ac98db50d7eb45c0137c446e4256
Elmelloi <seer10011278@outlook.com>
2025-07-01 23:17:58 +0800

    Finished Lab 1! Collatz works

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -8,7 +8,7 @@ public class Collatz {
         if(n==1){
             return 1;
         }
-        if (n % 2 == 1) {
+        if (n % 2 != 0) {
             return 3 * n + 1;
         } else {
             return n / 2;
